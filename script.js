5d0e83700dc2a9199f0cd81adc1be7e5a7e31e02
Victoriasteri <maryamhanifa90@gmail.com>
2022-11-14 15:31:20 +0300

    mobile menu

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-let hamburger = document.getElementsByClassName("header__titile");
+let hamburger = document.getElementById("hamburger");
 
-let menu = document.getElementsByClassName("mobile-menu");
+let menu = document.getElementById("mobileMenu");
 
-let closeBtn = document.getElementsByClassName("navbar__close-img");
+let closeBtn = document.getElementById("closeBtn");
+
+let navbar = document.getElementById("navbar");
 
 function openMenu() {
     hamburger.classList.toggle("hide");
     closeBtn.classList.toggle("display");
     menu.classList.toggle("menu-position");
+    navbar.classList.toggle("margin");
 };
 
 hamburger.addEventListener("click", openMenu);
